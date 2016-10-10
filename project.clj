405a912528db3a0a8a024e2852655566ffbd79ef
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-10 01:34:27 +0100

    Project infrastructure

---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject rm-hull/helpmate "0.1.0"
+  :description "A clojure library for emitting HTML (and XML) fragments"
+  :url "https://github.com/rm-hull/helpmate"
+  :license {
+    :name "The MIT License (MIT)"
+    :url "http://opensource.org/licenses/MIT"}
+  :dependencies [
+    [rm-hull/jasentaa "0.2.3"]]
+  :scm {:url "git@github.com:rm-hull/helpmate.git"}
+  :vcs :git
+  :source-paths ["src"]
+  :jar-exclusions [#"(?:^|/).git"]
+  :codox {
+    :source-paths ["src"]
+    :output-path "doc/api"
+    :source-uri "http://github.com/rm-hull/helpmate/blob/master/{filepath}#L{line}"  }
+  :min-lein-version "2.7.1"
+  :profiles {
+    :dev {
+      :global-vars {*warn-on-reflection* true}
+      :plugins [
+        [lein-codox "0.10.0"]
+        [lein-cljfmt "0.5.6"]
+        [lein-cloverage "1.0.7"]]
+      :dependencies [
+        [org.clojure/clojure "1.8.0"]]}})
+

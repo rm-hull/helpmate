1ede9f74cc79aaa31db93fe93f36a65a9e2e6636
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-11 00:35:21 +0100

    Removed temporary elms

--- test/helpmate/html_test.clj
+++ test/helpmate/html_test.clj
@@ -5,11 +5,6 @@
    [helpmate.core :refer :all]
    [helpmate.html :refer :all]))
 
-; Temporary...
-(defelem ^:non-void script)
-(defelem ul)
-(defelem span)
-
 (deftest check-embedded-tags
   (is (= "<html id=\"main\"><script type=\"text/javascript\" src=\"foobar.js\"></script><div class=\"side-panel\"><p>hello<span class=\"highlight\">world</span></p><ul><li>1</li><li>2</li><li>3</li></ul></div><div id=\"main\"></div></html>"
          (html {:id "main"}

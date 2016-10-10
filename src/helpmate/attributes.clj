a1ae608de354464df7a4686935f2cef781be5db2
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-10 23:39:37 +0100

    Id & class extractors

--- src/helpmate/attributes.clj
+++ src/helpmate/attributes.clj
@@ -5,3 +5,15 @@
 (defn expand [attrs]
   (when-not (empty? attrs)
     (str/join (for [[k v] attrs] (str " " (name k) "=\"" v "\"")))))
+
+(defn extract-id [kword]
+  (and
+   (keyword? kword)
+   (str/starts-with? (name kword) \#)
+   (-> (name kword) (subs 1))))
+
+(defn extract-classes [kword]
+  (and
+   (keyword? kword)
+   (str/starts-with? (name kword) \.)
+   (-> (name kword) (str/replace \. \space))))
--- test/helpmate/attributes_test.clj
+++ test/helpmate/attributes_test.clj
@@ -4,6 +4,20 @@
    [clojure.test :refer :all]
    [helpmate.attributes :as attrs]))
 
+(deftest check-extract-id
+  (is (false? (attrs/extract-id "fred")))
+  (is (false? (attrs/extract-id :fred)))
+  (is (false? (attrs/extract-id :.fred)))
+  (is (false? (attrs/extract-id {:id "fred"})))
+  (is (= "fred" (attrs/extract-id :#fred))))
+
+(deftest check-extract-classes
+  (is (false? (attrs/extract-classes "john.paul.george.ringo")))
+  (is (false? (attrs/extract-classes :john.paul.george.ringo)))
+  (is (false? (attrs/extract-classes :#john.paul.george.ringo)))
+  (is (false? (attrs/extract-classes {:class "john paul george ringo"})))
+  (is (= " john paul george ringo" (attrs/extract-classes :.john.paul.george.ringo))))
+
 (deftest check-expand-attrs
   (is (nil? (attrs/expand nil)))
   (is (nil? (attrs/expand [])))

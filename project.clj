33f15706e5ad5219e8531c60157ae74add2035d7
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-16 14:55:36 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rm-hull/helpmate "0.1.1"
+(defproject rm-hull/helpmate "0.1.2-SNAPSHOT"
   :description "A Clojure library for emitting SGML (HTML,SVG,XML) fragments"
   :url "https://github.com/rm-hull/helpmate"
   :license {
